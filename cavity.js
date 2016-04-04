7a48c0e86aaae0912a44e3f0e643676e80622cc1
Sebastian Steinlechner <sebastian.steinlechner@ligo.org>
2016-04-04 23:28:21 +0100

    initial commit

---
+++ cavity.js
@@ -0,0 +1,137 @@
+
+var Constants = {
+    c: 299792458,
+}
+
+function sqr(x) {
+    return x*x;
+}
+
+function waist_at_mirror(RoC1, RoC2, L, lambda0) {
+    if (RoC1 == L || RoC2 == L) {
+        w4 = L/(RoC1+RoC2-L);
+    } else {
+        w4 = (RoC2-L)/(RoC1-L) * L/(RoC1+RoC2-L);
+    }
+    return Math.pow(sqr(lambda0*RoC1/Math.PI) * w4, 0.25);
+}
+
+class Cavity {
+    constructor(R1, R2, L, RoC1, RoC2) {
+        this.r1 = Math.sqrt(R1);
+        this.r2 = Math.sqrt(R2);
+        this.L = L;
+        this.RoC1 = RoC1;
+        this.RoC2 = RoC2;
+    }
+
+    get FSR() {
+        return Constants.c / (2*this.L);
+    }
+
+    get T() {
+        return 1.0 - this.R;
+    }
+
+    get R() {
+        return sqr((this.r1 - this.r2) / (1-this.r1*this.r2));
+    }
+
+    get F() {
+        return Math.PI * Math.sqrt(this.r1*this.r2)/(1-this.r1*this.r2);
+    }
+
+    get buildup() {
+        return (1.0 - sqr(this.r1))/sqr(1-this.r1*this.r2);
+    }
+
+    get g1() {
+        return (1-this.L / sqr(this.RoC1))
+    }
+
+    get g2() {
+        return (1-this.L / sqr(this.RoC2))
+    }
+
+    get g1g2() {
+        return this.g1*this.g2;
+    }
+
+    is_stable() {
+        var g1g2 = this.g1g2;
+        if (g1g2 < 0 || g1g2 > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    beamradius_at_M1(lambda0) {
+        return waist_at_mirror(this.RoC1, this.RoC2, this.L, lambda0);
+    }
+
+    beamradius_at_M2(lambda0) {
+        return waist_at_mirror(this.RoC2, this.RoC1, this.L, lambda0);
+    }
+
+    waist(lambda0) {
+        if (this.RoC1 == this.L && this.RoC2 == this.L) {
+            return Math.sqrt(lambda0*this.L/2/Math.PI);
+        } else if (this.RoC1 == this.L || this.RoC2 == this.L) {
+            console.log('RoC1 == L or RoC2 == L, but not both. Formula fails here :-(');
+            return -1;
+        }
+        var N = this.L*(this.RoC1-this.L)*(this.RoC2-this.L)*(this.RoC1+this.RoC2-this.L);
+        var D = sqr(this.RoC1+this.RoC2-2*this.L);
+
+        return Math.pow(sqr(lambda0/Math.PI)*N/D, 0.25);
+    }
+}
+
+function $(id)
+{
+    return document.getElementById(id);
+}
+
+function update()
+{
+    clear_results();
+    cav = new Cavity(parseFloat($('cav_R1').value) / 100.0,
+                     parseFloat($('cav_R2').value) / 100.0,
+                     parseFloat($('cav_L').value),
+                     parseFloat($('cav_RoC1').value),
+                     parseFloat($('cav_RoC2').value));
+    var lambda0 = parseFloat($('cav_lambda0').value) * 1e-9;
+    
+    log_result('FSR', cav.FSR.toExponential(3), 'Hz');
+    log_result('Finesse', cav.F.toExponential(3), '');
+    log_result('Power build-up', cav.buildup.toExponential(3), '');
+    log_result('Reflected power', (cav.R * 100).toFixed(3), '%');
+    log_result('Transmitted power', (cav.T * 100).toFixed(3), '%');
+    var is_stable = cav.is_stable();
+    log_result('Cavity stable', is_stable ? 'yes' : 'no', '');
+    if (is_stable) {
+        log_result('g1*g2', cav.g1g2.toFixed(4), '')
+        log_result('Beam waist', (cav.waist(lambda0)*1e6).toFixed(1), '&mu;m');
+        log_result('Beam radius at M1', (cav.beamradius_at_M1(lambda0)*1e6).toFixed(1), '&mu;m');
+        log_result('Beam radius at M2', (cav.beamradius_at_M2(lambda0)*1e6).toFixed(1), '&mu;m');
+    }
+}
+
+function log_result(desc, value, unit)
+{
+    out = get_result_window();
+    out.innerHTML += '<span class="desc">'+desc+':</span> '
+        + '<span class="value">' + value + '</span> <span class="unit">'
+        + unit + '</span><br />';
+}
+
+function clear_results()
+{
+    out = get_result_window();
+    out.innerHTML = '';
+}
+
+function get_result_window()
+{
+    return $('results');
+}
